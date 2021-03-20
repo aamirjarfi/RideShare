fd506a37e51329a22bd74ff2450e79fb50fb3219
Coding Bird <Codingbird@yahoo.com>
2021-03-20 23:38:56 +0600

    unwanted code removed

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import "./Login.css";
 import { Container } from "react-bootstrap";
-import { Link } from "react-router-dom";
 import LoginContent from "../LoginContent/LoginContent";
 const Login = () => {
   return (
