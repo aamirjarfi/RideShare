e23ad1857b05ea0dc0d038231cda668dd39bd27c
Coding Bird <Codingbird@yahoo.com>
2021-03-21 23:34:16 +0600

    images changed

--- src/Resources/JsonData/JsonData.js
+++ src/Resources/JsonData/JsonData.js
@@ -2,12 +2,12 @@ export const jsonData = [
   {
     id: "60545a0eb4f94c7c2ab8372b",
     name: "Bike",
-    picture: "https://i.ibb.co/PgM1h2P/bike.png",
+    picture: "https://i.ibb.co/wQXFnDC/bike2.jpg",
     rideCost: "89",
   },
   {
     id: "60545a0e3f282be385a81348",
-    picture: "https://i.ibb.co/d4yZjVD/bus.png",
+    picture: "https://i.ibb.co/MyHCkBk/bus2.png",
     name: "Bus ",
     rideCost: "45",
   },
@@ -19,7 +19,8 @@ export const jsonData = [
   },
   {
     id: "60545a0e0a3f158102cd34f5",
-    picture: "https://i.ibb.co/T8z0mmh/train.png",
+    // picture: "https://i.ibb.co/T8z0mmh/train.png",
+    picture: "https://i.ibb.co/m8nXQ6x/train2.jpg",
     name: "Train",
     rideCost: "25",
   },
